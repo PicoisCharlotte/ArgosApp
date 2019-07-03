7f0c55c039a82c7fbd7efe37faa7aebbaaf48d73
PicoisCharlotte <charlotte.picois.sio@gmail.com>
2019-07-03 09:30:41 +0200

    add btn co/deco

--- app/src/main/java/app/argos/com/argosapp/MainActivity.kt
+++ app/src/main/java/app/argos/com/argosapp/MainActivity.kt
@@ -2,9 +2,11 @@ package app.argos.com.argosapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import app.argos.com.argosapp.Fragment.HomeFragment
 import app.argos.com.argosapp.Fragment.RobotsFragment
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
 
     private val client = OkHttpClient()
+    private var isConnected: Boolean = false
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -39,6 +42,19 @@ class MainActivity : AppCompatActivity() {
                 }
                 return@OnNavigationItemSelectedListener true
             }
+            R.id.connexion -> {
+
+                val connexion = tabbar.menu.findItem(R.id.connexion)
+                if (Statics.IS_CONNECTED) {
+                    connexion.setTitle(resources.getString(R.string.connexion))
+                    Statics.IS_CONNECTED = false;
+                    val intent = Intent(this, ConnexionActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    val intent = Intent(this, ConnexionActivity::class.java)
+                    startActivity(intent)
+                }
+            }
         }
         false
     }
@@ -47,10 +63,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*val isConnected = savedInstanceState?.get("statics")
+        if(isConnected != null){
+            Statics.IS_CONNECTED = isConnected as Boolean
+        }
+        Log.d("MainAct", "RESTOOOOOORE !!!!!!! ")*/
+
         this.tabbar.setOnNavigationItemSelectedListener (mOnNavigationItemSelectedListener)
 
         val homeFragment = HomeFragment.newInstance()
         openFragment(homeFragment)
+
+
+        val connexion = tabbar.menu.findItem(R.id.connexion)
+        if (Statics.IS_CONNECTED) {
+            connexion.setTitle(resources.getString(R.string.deconnexion))
+        } else {
+            connexion.setTitle(resources.getString(R.string.connexion))
+        }
+    }
+
+    /*public override fun onSaveInstanceState(): Parcelable? {
+        val savedState = android.app.Fragment.SavedState(super.onSaveInstanceState())
+        savedState.isExpanded = isExpanded
+        return savedState
+    }
+    public override fun onRestoreInstanceState(state: Parcelable) {
+        if (state is android.app.Fragment.SavedState) {
+            super.onRestoreInstanceState(state.
+                    superState)
+            isExpanded = state.isExpanded
+        } else {
+            super.onRestoreInstanceState(state)
+        }
+    }*/
+
+    override fun onSaveInstanceState(outState: Bundle?){
+        super.onSaveInstanceState(outState)
+        outState?.putCharSequence("statics", Statics.IS_CONNECTED.toString())
+        Log.d("MainAct", "ON SAVED INSTANCE !! ")
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Statics.IS_CONNECTED = savedInstanceState?.get("statics") as Boolean
+        Log.d("MainAct", "RESTOOOOOORE !!!!!!! ")
     }
 
     private fun openFragment(fragment: Fragment) {
